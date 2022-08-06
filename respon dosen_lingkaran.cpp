cb92847efa8b9d8b99a29ee99a2c3fc7fb3aaf9f
ahmadbariqn <95735924+bariqgagah@users.noreply.github.com>
2022-08-07 01:41:12 +0800

    Add files via upload

---
+++ respon dosen_lingkaran.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float phi=3.1415,luas;
+    int r;
+    cout<<"Masukkan nilai jari-jari : ";cin>>r;
+    luas=phi*r*r;
+    cout<<"Luas lingkaran = "<<luas;
+}
